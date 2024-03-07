ffa14b2dcbd1a2830ec4ac4732b8fb0d44af4d99
Jason Yue <kymotsujason@gmail.com>
2024-03-06 21:39:52 -0800

    Add a file structure to frontend and remove warning

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import "../assets/Navbar.css";
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <h1>This is a navbar</h1>
+        </div>
+    );
+}
+
+export default Navbar;
--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "../components/Navbar.js";
+import logo from '../assets/logo.svg';
+
+import '../assets/App.css';
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/views/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
